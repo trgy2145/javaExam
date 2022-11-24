99f573b40203a32a2a00310c7fedc0ff458a1167
trgy2145 <trgy2145@gmail.com>
2022-11-24 20:05:20 +0100

    Arrays V01

--- JavaPractice/src/exam20_arrays/C01_Arrays.java
+++ JavaPractice/src/exam20_arrays/C01_Arrays.java
@@ -1,4 +1,16 @@
 package exam20_arrays;
 
 public class C01_Arrays {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3},{5,6,7},{8,9}};
+        int sum = 0;
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+
+            }
+
+        }
+        System.out.println(sum);
+    }
 }
