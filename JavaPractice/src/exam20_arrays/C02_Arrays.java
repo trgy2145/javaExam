d92336afa5f8578dcc6c5fe6a4727ac543360228
trgy2145 <trgy2145@gmail.com>
2022-11-24 20:42:38 +0100

    V02

---
+++ JavaPractice/src/exam20_arrays/C02_Arrays.java
@@ -0,0 +1,30 @@
+package exam20_arrays;
+
+
+import java.util.Arrays;
+
+public class C02_Arrays {
+    public static void main(String[] args) {
+        // aynı indexe sahip elementleri toplayıp yeni arraya atayalım
+        int [][] arr = {{3,4,5},{2,3,6,7}}; // input
+        // output [5,7,11]
+        int len;
+
+        System.out.println(Arrays.toString(arr[1]));
+        if(arr[0].length >= arr[1].length){
+            len = arr[1].length;
+        }
+        else{
+            len = arr[0].length;
+        }
+        int[] newArray = new int[len];
+        System.out.println(len);
+
+      for (int i = 0; i < len; i++) {
+                newArray[i] = arr[0][i]+arr[1][i];
+
+         }
+        System.out.println(Arrays.toString(newArray));
+
+    }
+}
